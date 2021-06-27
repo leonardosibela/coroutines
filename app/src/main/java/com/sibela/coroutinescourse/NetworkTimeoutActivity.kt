558bfb52f63214b8e3d41a7b853de1efd83ec6c6
leonardosibela <leonardo_sibela@hotmail.com>
2021-06-26 22:40:33 -0300

    Implementing Network Timeout example

--- app/src/main/java/com/sibela/coroutinescourse/NetworkTimeoutActivity.kt
+++ app/src/main/java/com/sibela/coroutinescourse/NetworkTimeoutActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sibela.coroutinescourse.databinding.ActivityNetworkTimeoutBinding
+import kotlinx.coroutines.*
 
 class NetworkTimeoutActivity : AppCompatActivity() {
+
+    private companion object {
+        const val TIMEOUT = 1_000L
+    }
 
     private lateinit var binding: ActivityNetworkTimeoutBinding
 
@@ -17,6 +22,22 @@ class NetworkTimeoutActivity : AppCompatActivity() {
     }
 
     private fun onGetResultClicked(view: View) {
-        
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = withTimeoutOrNull(TIMEOUT) { getResult() }
+            if (result == null) {
+                displayTextOnMainThread("getResult timed out")
+            } else {
+                displayTextOnMainThread(result)
+            }
+        }
+    }
+
+    private suspend fun displayTextOnMainThread(text: String) = withContext(Dispatchers.Main) {
+        binding.resultText.text = text
+    }
+
+    private suspend fun getResult(): String {
+        delay(2_000)
+        return "Some result"
     }
 }
